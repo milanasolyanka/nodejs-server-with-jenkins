4dbdfafac6b96dc9a71218fcb1752e0b41c28ca9
milanasolyanka <tupeykoma@gmail.com>
2025-10-15 17:53:18 +0700

    added message broker rabbitmq and dockerized everything

--- about/index.js
+++ about/index.js
@@ -2,9 +2,40 @@ const express = require("express");
 const app = express();
 const port = 3002;
 
+let amqp = require("amqplib/callback_api");
+amqp.connect("amqp://rabbitmq", function (error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(function (error1, channel) {
+    if (error1) {
+      throw error1;
+    }
+
+    let queue = "funfacts";
+    channel.assertQueue(queue, {
+      durable: false,
+    });
+
+    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+    channel.consume(
+      queue,
+      function (msg) {
+        console.log(" [x] Received %s", msg.content.toString());
+      },
+      {
+        noAck: true,
+      }
+    );
+  });
+});
+
 app.get("/", (req, res) => {
   console.log("Received a get request for /");
-  res.send("Ahh, it will be a long story bout me ;)");
+  // тут надо делать запрос на profiles, чтобы он возвращал инфо про importantUser
+  res.send(
+    `Ahh, it will be a long story bout me ;)\nBtw, user ${importantUserID} was very important to our platform, so here is some info abt him: `
+  );
 });
 
 app.listen(port, () => {
--- profiles/index.js
+++ profiles/index.js
@@ -2,25 +2,64 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
+let amqp = require("amqplib/callback_api");
+amqp.connect("amqp://rabbitmq", function (error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(function (error1, channel) {
+    if (error1) {
+      throw error1;
+    }
+
+    let queue = "funfacts";
+    channel.assertQueue(queue, {
+      durable: false,
+    });
+
+    // msg = "Hello world";
+    // channel.sendToQueue(queue, Buffer.from(msg));
+    // console.log(" [x] Sent %s", msg);
+
+    setInterval(() => {
+      let msg = generateFunfact(getRandomArbitrary(1, 300));
+      channel.sendToQueue(queue, Buffer.from(msg));
+      console.log(`Sent message "${msg}" to queue`);
+    }, 10 * 1000);
+  });
+});
+
 app.get("/", (req, res) => {
   console.log("Received a get request");
   res.send("Howdy, I am a server ;)");
 });
 
 app.get("/:id", (req, res) => {
-  const profileId = req.params.id;
-  console.log(`Received a get request for profile ID: ${profileId}`);
-  res.send(`Howdy! You asked bout a person w profile id: ${profileId}`);
+  const profileID = req.params.id;
+  console.log(`Received a get request for profile ID: ${profileID}`);
+  res.send(getProfileInfoByID(profileID));
 });
 
 app.post("/:id", (req, res) => {
-  const profileId = req.params.id;
-  console.log(`Received a post request for profile ID: ${profileId}`);
+  const profileID = req.params.id;
+  console.log(`Received a post request for profile ID: ${profileID}`);
   res.send(
-    `Howdy, I jus received your POST request on a profile w id: ${profileId} ;)`
+    `Howdy, I jus received your POST request on a profile w id: ${profileID} ;)`
   );
 });
 
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
 });
+
+function getProfileInfoByID(profileID) {
+  return `Howdy! You asked bout a person w profile id: ${profileID}`;
+}
+
+function generateFunfact(profileID) {
+  return `Fun fact about user ${profileID}: they are very smart and attractive <3`;
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.trunc(Math.random() * (max - min) + min);
+}
